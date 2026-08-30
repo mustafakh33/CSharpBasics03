9420071b22ab2b26389358f1909857581177b8c6
Mustafa Khaled <mustafakh565@gmail.com>
2026-08-30 23:04:35 +0300

    Question02

--- CSharpBasics03/Program.cs
+++ CSharpBasics03/Program.cs
@@ -14,6 +14,25 @@
                 Console.WriteLine("You can borrow this book");
             }
             #endregion
+
+            #region Question02
+            // 2. Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+            string title = "Refactoring";
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great choice!");
+                    break;
+
+                case "Refactoring":
+                    Console.WriteLine("Nice pick!");
+                    break;
+
+                default:
+                    Console.WriteLine("Never heard of it");
+                    break;
+            }
+            #endregion
         }
     }
 }
